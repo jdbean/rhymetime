a580c1434832b20f70cbeb8acc7e12e447d272ee
J.D. Bean <jonathandbean@gmail.com>
2018-08-31 17:06:03 -0400

    Workaround bug in pronouncing, new pronunciation feature, refactoring
    Discovered bug in pronouncing caused rhymes method to return only the
    rhymes for the first pronunciation contrary to documentation.
    Created new method to achieve the intented behavior.
    
    Created new endpoint to serve information on pronunciation variants
    
    Added feature to obtain rhymes for only a specific pronunciation
    variant.
    
    Refactored code for validation of english words into helper method

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
+from itertools import chain
 import random
 import re
 import pronouncing
@@ -16,6 +17,64 @@ def not_found(error):
 @APP.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+# Helper function for attempting to convert a string to an integer
+def try_int(string):
+    try:
+        integer = int(string)
+        return integer
+    except ValueError:
+        return False
+# Helper function to find all rhymes for all known pronunciations of a word
+# Achieves advertised functionality of pronouncing.rhymes() at
+# https://pronouncing.readthedocs.io/en/latest/tutorial.html#rhyme
+# Docs state that function will return rhymes for all possible pronunciations
+# As written, function returns rhymes only for the first pronunciation.
+#
+# NOTE: THIS FUNCTION IS BASED ON CODE FROM PRONOUNCING
+# `pronouncingpy/pronouncing/__init__.py` line 203
+def all_rhymes(word):
+    # find list of all pronunciations for word
+    phones = pronouncing.phones_for_word(word)
+    # setup empty list to hold matching rhyme array for mutliple
+    # pronunciations
+    combined_rhymes = []
+    if phones:
+        # add the lists of rhyming words for each pronunciation to new list.
+        for element in phones:
+            combined_rhymes.append([w for w in pronouncing.rhyme_lookup.get(
+                                   pronouncing.rhyming_part(element), [])
+                                   if w != word])
+        # flatten list of lists
+        combined_rhymes = list(chain.from_iterable(combined_rhymes))
+        # sort the new combined list
+        combined_rhymes.sort()
+        return combined_rhymes
+    else:
+        return []
+
+def validate_english_word(args):
+    # store value of 'word' key in query to variable
+    word = args.get('word')
+    # store value of regex pattern for any non alpha character
+    valid_char_pattern = r'[^A-Za-z\']'
+    # check if word non-nil and composed of only valid english-language alpha
+    # chars. Regex test is applied her to avoid uneeded dictionary lookup and
+    # to distinguish 'Not Found' (404) from 'Bad Request' (400)
+    if not word or re.search(valid_char_pattern, word):
+        abort(400)
+    # Set the value of 'word' to all lowercase
+    word = word.lower()
+    # Initialize pronouncing's lookup object collection if not yet initialized
+    pronouncing.init_cmu()
+    # If the word isn't in the CMU pronouncing dictionary respond with 404
+    # This is required because the pronouncing library does not offer a
+    # distinction between word pronunciation unknown (i.e. rhyme matches
+    # unkown) and word found but no rhymes found (i.e. believed to rhyme
+    # with no other word in dictionary)
+    if not word in pronouncing.lookup:
+        abort(404)
+    return word
 
 ## Randomchoice route for 'post' action.
 #
@@ -49,41 +108,49 @@ def random_word_choice():
     return jsonify({'word': random_word_chosen})
 
 ## rhymes route for 'get' action.
-
 @APP.route('/api/v1.0/words/rhymes', methods=['get'])
 # Accepts an english word and returns that word along with an array
-# of words that rhymes with potential pronounciations of that word.
+# of words that rhymes with potential pronunciations of that word.
 # Inclusion in the CMU pronouncing dictionary is assumed to be
 # necessary and sufficient to classify a string as an 'english word'
 def get_rhymes():
-    # check if the query string contains a 'query' string and otherwise
-    # respond 400
-    if 'query' not in request.args:
-        abort(400)
-    # store value of 'query' key in query to variable
-    word = request.args.get('query')
-    # store value of regex pattern for any non alpha character
-    valid_char_pattern = r'[^A-Za-z\']'
-    # check if word non-nil and composed of only valid english-language alpha
-    # chars. Regex test is applied her to avoid uneeded dictionary lookup and
-    # to distinguish 'Not Found' (404) from 'Bad Request' (400)
-    if not word or re.search(valid_char_pattern, word):
-        abort(400)
-    # Set the value of 'word' to all lowercase
-    word = word.lower()
-    # Initialize pronouncing's lookup object collection if not yet initialized
-    pronouncing.init_cmu()
-    # If the word isn't in the CMU pronouncing dictionary respond with 404
-    # This is required because the pronouncing library does not offer a
-    # distinction between word pronounciation unknown (i.e. rhyme matches
-    # unkown) and word found but no rhymes found (i.e. believed to rhyme
-    # with no other word in dictionary)
-    if not word in pronouncing.lookup:
-        abort(404)
+    # Validate the requested english word and if return, store word in variable
+    word = validate_english_word(request.args)
     # Use pronouncing to find potential rhymes and store in variable rhymes
-    rhymes = pronouncing.rhymes(word)
-    # Return json to client with rhymes array and the query
-    return jsonify({'ryhmes': rhymes, 'query': word})
+    pronounce_str = request.args.get('pronunciation_id')
+
+    if pronounce_str:
+        # Assign pronounce to return of try_int() [int or false]
+        pronounce_id = try_int(pronounce_str)
+        # find list of all pronunciations for word
+        pronunciations = pronouncing.phones_for_word(word)
+        # if pronounce_id is not an int respond 400 (conversion failed)
+        if not isinstance(pronounce_id, int):
+            abort(400)
+        # is pronounce_id a valid pronunciation index. Return 404 if not
+        if pronounce_id not in range(len(pronunciations)):
+            abort(404)
+        # identify the portion of specified pronunciation relevant to rhyming
+        rhyme_sound = pronouncing.rhyming_part(pronunciations[pronounce_id])
+        # store list of rhyming words
+        rhymes = pronouncing.search(rhyme_sound + "$")
+    else:
+        # if no pronunciation_id is provided by user, store list of rhymes for
+        # all pronunciatons of the word
+        rhymes = all_rhymes(word)
+    # Return json to client with rhymes array and the queried word
+    return jsonify({'rhymes': rhymes, 'word': word})
+
+
+
+@APP.route('/api/v1.0/words/pronunciations', methods=['get'])
+def get_pronunciations():
+    # Validate the requested english word and if return, store word in variable
+    word = validate_english_word(request.args)
+    # find list of all pronunciations for word
+    pronunciations = pronouncing.phones_for_word(word)
+    # return list pronunciation phones and the queried word to user
+    return jsonify({'pronunciations': pronunciations, 'word': word})
 
 if __name__ == '__main__':
     APP.run(debug=True)
